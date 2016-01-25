fe3a59a2b2ab88239e50bc5cbf6cb1ff34a0078b
stamaniorec <stamaniorec@gmail.com>
2016-01-25 21:40:43 +0200

    Improved task 1.

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,12 +1,12 @@
+RATES = {
+  usd: 1.7408,
+  eur: 1.9557,
+  gbp: 2.6415,
+  bgn: 1.0,
+}.freeze
+
 def convert_to_bgn(price, currency)
-  rates = {
-    usd: 1.7408,
-    eur: 1.9557,
-    gbp: 2.6415,
-    bgn: 1.0
-  }
-  rate = rates[currency]
-  (rate * price).round(2)
+  (RATES[currency] * price).round(2)
 end
 
 def compare_prices(price_a, currency_a, price_b, currency_b)
